8e638405722f659485bc6f2d9d08b08052db0d77
Surin Ahn <slahn@princeton.edu>
2017-04-19 14:42:27 -0400

    added controls.py

---
+++ controls.py
@@ -0,0 +1,69 @@
+# Functions for controlling the claws
+
+# Turn front side CW
+def turnF:
+    # Turn front claw 90 degrees CW
+    # Ungrip claw
+    # Turn front claw 90 degrees CCW
+    # Grip claw
+
+# Turn front side CCW
+def turnFp:
+    # Turn front claw 90 degrees CCW
+    # Ungrip claw
+    # Turn front clas 90 degrees CW
+    # Grip claw
+
+# Turn right side CW
+def turnR:
+    # Turn right claw 90 degrees CW
+    # Ungrip claw
+    # Turn right claw 90 degrees CCW
+    # Grip claw
+
+# Turn right side CCW
+def turnRp:
+    # Turn right claw 90 degrees CCW
+    # Ungrip claw
+    # Turn right claw 90 degrees CW
+    # Grip claw
+
+# Turn left side CW
+def turnL:
+    # Turn left claw 90 degrees CW
+    # Ungrip claw
+    # Turn left claw 90 degrees CCW
+    # Grip claw
+
+# Turn left side CCW
+def turnLp:
+    # Turn left claw 90 degrees CCW
+    # Ungrip claw
+    # Turn left claw 90 degrees CW
+    # Grip claw
+
+# Turn up side CW
+def turnU:
+
+# Turn up side CCW
+def turnUp:
+
+# Turn down side CW
+def turnD:
+
+# Turn down side CCW
+def turnDp:
+
+# Turn back side CW
+def turnB:
+    # Turn up back 90 degrees CW
+    # Ungrip claw
+    # Turn up back 90 degrees CCW
+    # Grip claw
+
+# Turn back side CCW
+def turnBp:
+    # Turn back claw 90 degrees CCW
+    # Ungrip claw
+    # Turn back claw 90 degrees CW
+    # Grip claw
