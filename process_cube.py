8612b8f110235f7d343321c37b916e763a109e3e
vivianmo <vivianmo@nat-oitwireless-inside-vapornet100-b-25208.Princeton.EDU>
2017-04-19 23:38:45 -0400

    works

--- process_cube.py
+++ process_cube.py
@@ -1,11 +1,11 @@
 import os, sys, subprocess
 import numpy as np
 
-base_x = 100
-base_y = 80
+base_x = 110
+base_y = 50
 
-side = 40 
-error = 30
+side = 50
+offset = 30
 
 data = open(sys.argv[1], "r")
 
@@ -16,8 +16,15 @@ for i in range(0,9):
 
 hits = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 
-def contains(mid_x, mid_y, x, y, w, h): 
-	return (mid_x-x <error and mid_y-y < error and h*w>100)
+def contains(sig, mid_x, mid_y, x, y): 
+	offset_x = mid_x-x; 
+	offset_y = mid_y-y; 
+	print str(sig) + ":" + str(mid_x) + ":" +str(mid_y)
+	if offset_x < 0 or offset_y < 0: 
+		return False 
+	if offset_x < offset and offset_y < offset: 
+		print str(sig) + ":" + str(offset_x) + ":" +str(offset_y)
+	return (offset_x < offset and offset_y < offset)
 
 for line in data.readlines(): 
 	split = line.split() 
@@ -32,10 +39,11 @@ for line in data.readlines():
 		for i in range(0,3): 
 			num = j*3 + i 
 
-			mid_x = base_x+i*side
-			mid_y = base_y+j*side
+			mid_x = base_x+(i+.5)*side
+			mid_y = base_y+(j+.5)*side
 
-			if contains(mid_x, mid_y, x, y, w, h):
-				blocks[num][sig-1] += 1
+			if w*h > 100: 
+				if contains(sig, mid_x, mid_y, x, y):
+					blocks[num][sig-1] += 1
 
 print blocks
