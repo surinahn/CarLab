9a61b5625a79a881b50a60494edb1a8a89839cb3
vivianmo <vivianmo@nat-oitwireless-inside-vapornet100-b-25208.Princeton.EDU>
2017-05-08 13:38:09 -0400

    adjust

--- code/process_cube.py
+++ code/process_cube.py
@@ -6,7 +6,7 @@ base_x = 120
 base_y = 50
 
 side = 50
-offset = 30
+offset = 40
 
 data = open(sys.argv[1], "r")
 
@@ -28,6 +28,27 @@ def contains(sig, mid_x, mid_y, x, y):
 	# 	print str(sig) + ":" + str(offset_x) + ":" +str(offset_y)
 	return (offset_x < offset and offset_y < offset)
 
+min_x = 500 
+min_y = 500
+for i in range(0,500): 
+	line = data.readline() 
+	split = line.split() 
+	split = [int(s) for s in split]
+	sig = split[0]
+	x = split[1]
+	y = split[2]
+	w = split[3]
+	h = split[4]
+	if w*h>400 and x < min_x and y < min_y:
+		min_x = x 
+		min_y = y 
+
+base_x = min_x
+base_y = min_y
+data.seek(0)
+print base_x
+print base_y
+
 print "begin"
 for line in data.readlines(): 
 	split = line.split() 
@@ -45,7 +66,7 @@ for line in data.readlines():
 			mid_x = base_x+(i+.5)*side
 			mid_y = base_y+(j+.5)*side
 
-			if w*h > 100: 
+			if w*h > 200: 
 				if contains(sig, mid_x, mid_y, x, y):
 					blocks[num][sig-1] += 1
 
