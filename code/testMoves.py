7431ad4142068bc000e4f6e0624a14818d4668d7
vivianmo <vivianmo@nat-oitwireless-inside-vapornet100-b-25208.Princeton.EDU>
2017-05-11 22:37:35 -0400

    rotations

--- code/testMoves.py
+++ code/testMoves.py
@@ -13,22 +13,34 @@ while userInput != 'c':
 
 close()
 
-##print "0"
-##rotate0()
-##sleep(2)
-##print "1" 
-##rotate1()
-##sleep(2)
-##print "2" 
-##rotate2()
-##sleep(2)
-##print "1" 
-##rotate1()
-##sleep(2)
+print "0"
+rotate0()
+sleep(2)
+print "1" 
+rotate1()
+sleep(2)
+print "2" 
+rotate2()
+sleep(2)
+print "1" 
+rotate1()
+sleep(2)
+print "2" 
+rotate2()
+sleep(2)
+print "1" 
+rotate1()
+sleep(2)
+print "2" 
+rotate2()
+sleep(2)
+print "1" 
+rotate1()
+sleep(2)
 
-turnF(2)
-turnR(2)
-turnB(2)
-turnL(2)
-turnU(2)
-turnD(2)
+# turnF(2)
+# turnR(2)
+# turnB(2)
+# turnL(2)
+# turnU(2)
+# turnD(2)
