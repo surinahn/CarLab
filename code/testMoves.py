678c840d62b4c72c4300664a35fa921fe8458562
vivianmo <vivianmo@nat-oitwireless-inside-vapornet100-b-25208.Princeton.EDU>
2017-05-12 01:22:40 -0400

    exercises

--- code/testMoves.py
+++ code/testMoves.py
@@ -26,35 +26,35 @@ close()
 # 	B_n_CCW()
 # 	sleep(3)
 
-print "0"
-rotate0()
-sleep(2)
-print "1" 
-rotate1()
-sleep(2)
-print "2" 
-rotate2()
-sleep(2)
-print "1" 
-rotate1()
-sleep(2)
-print "2" 
-rotate2()
-sleep(2)
-print "1" 
-rotate1()
-sleep(2)
-print "2" 
-rotate2()
-sleep(2)
-print "1" 
-rotate1()
-sleep(2)
+# print "0"
+# rotate0()
+# sleep(2)
+# print "1" 
+# rotate1()
+# sleep(2)
+# print "2" 
+# rotate2()
+# sleep(2)
+# print "1" 
+# rotate1()
+# sleep(2)
+# print "2" 
+# rotate2()
+# sleep(2)
+# print "1" 
+# rotate1()
+# sleep(2)
+# print "2" 
+# rotate2()
+# sleep(2)
+# print "1" 
+# rotate1()
+# sleep(2)
 
-# turnR(0)
-# turnF(2)
-# turnR(2)
-# turnB(2)
-# turnL(2)
-# turnU(2)
-# turnD(2)
+turnR(0)
+turnF(2)
+turnR(2)
+turnB(2)
+turnL(2)
+turnU(2)
+turnD(2)
