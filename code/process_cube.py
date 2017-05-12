98f28f3636f9ec42caa8c4d96a2a71181b2a072a
vivianmo <vivianmo@nat-oitwireless-inside-vapornet100-b-25208.Princeton.EDU>
2017-05-11 22:21:54 -0400

    process cube fix

--- code/process_cube.py
+++ code/process_cube.py
@@ -29,8 +29,8 @@ def contains(sig, mid_x, mid_y, x, y):
 	# 	print str(sig) + ":" + str(offset_x) + ":" +str(offset_y)
 	return (offset_x < offset and offset_y < offset)
 
-cutoff = 70
-cutoff_x = 50
+cutoff = 100
+cutoff_x = 60
 cutoff_y = 50
 x_over = 0 
 x_under = 0 
@@ -56,15 +56,18 @@ for i in range(0,300):
 			if y < cutoff_y: 
 				y_under += 1
 
-# print x_under
-# print x_over 
-# print y_under
-# print y_over
+print "x under/over"
+print x_under
+print x_over 
+print "y under/over"
+print y_under
+print y_over
 
-if x_under < 100: 
-	cutoff_x = 70
+if x_under < 50: 
+	cutoff_x = 100
 if y_under < 100: 
-	cutoff_y = 70
+	cutoff_y = 100
+
 
 min_xs = [] 
 min_ys = []
@@ -87,9 +90,8 @@ for i in range(0,500):
 base_x = np.median(min_xs)
 base_y = np.median(min_ys)
 data.seek(0)
-# print min_xs
-# print base_x
-# print base_y
+print base_x
+print base_y
 
 #print "begin"
 for line in data.readlines(): 
@@ -113,16 +115,16 @@ for line in data.readlines():
 				if contains(sig, mid_x, mid_y, x, y):
 					blocks[num][sig-1] += 1
 
-# print blocks
+print blocks
 max_indices = [letters[b.index(max(b))] for b in blocks]
-# print "max_indices"
-# print max_indices
+print "max_indices"
+print max_indices
 ##process for white
 for i in range(0,9): 
 	if max(blocks[i]) < 5: 
 		max_indices[i] = letters[4]
 string = ''.join(max_indices)
-# print string
+print string
 
 
 
